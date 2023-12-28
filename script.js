44a692780f9c2e3f79edf4c5e53e2ac3d2c7c64f
UltraS <40329996+UltraSpeci@users.noreply.github.com>
2023-12-28 13:26:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function main() {
     if(sac1_value <= sac2_value) {
         var sac1_amount = calculate(value, amount, sac1_value, sac2_value);
         var sac2_amount = amount - sac1_amount;
-        document.getElementById('sac1_amount').value = `sac 1: ${sac1_amount}`;
-        document.getElementById('sac2_amount').value = `sac 2: ${sac2_amount}`;
+        document.getElementById('sac1_amount').value = sac1_amount;
+        document.getElementById('sac2_amount').value = sac2_amount;
     } else {
         var sac2_amount = calculate(value, amount, sac2_value, sac1_value);
         var sac1_amount = amount - sac2_amount;
-        document.getElementById('sac1_amount').value = `sac 1: ${sac1_amount}`;
-        document.getElementById('sac2_amount').value = `sac 2: ${sac2_amount}`;
+        document.getElementById('sac1_amount').value = sac1_amount};
+        document.getElementById('sac2_amount').value = sac2_amount;
     }
 }
 function calculate(value, amount, sac1_value, sac2_value) {
